4cada7edbda92a95609742ed96395c3570428200
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-22 15:00:41 +0530

    add devcamper_api master

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bootcampRouter = require('./routes/bootcamps');
 const morgan =require('morgan');
+const connectDB = require('./config/db');
+dotenv.config({ path: '../config/config.env' });
 
-dotenv.config({ path: './config/config.env' });
-
+connectDB();
 const app = express();
 
 //app.use(logger); // Use the external logger middleware
