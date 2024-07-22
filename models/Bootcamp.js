5ce90c17a7e2c7aeea6db5dc231bd9c371fc279e
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-22 16:11:09 +0530

    add devcamper_api master

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -31,6 +31,25 @@ const BootcampSchema = new mongoose.Schema({
     address: {
         type: String,
         required: [true, 'Please add an address']
+    },
+    location: {
+        // GeoJSON Point
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            index: '2dsphere'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String
     }
 });
 
