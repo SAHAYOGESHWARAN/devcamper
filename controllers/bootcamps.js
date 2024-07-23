6d109b04a7a1aaef3bed032b0d258d043c64d8f4
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-23 08:42:22 +0530

    add devcamper_api master

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,9 +1,12 @@
+const Bootcamp = require('../models/Bootcamp');
+
+
 //@desc Get all bootcamps
 //@route GET /api/v1/bootcamps
 //@access pubilc
 exports.getBootcamps =(req, res, next) => {
-    res
-    .status(200)
+   console.log(req.body);
+    res.status(200)
     .json({ success: true, msg: 'Show all bootcamps'});
 }
 
