d6ffcd7e494df10998c9618c6cdc799ca660fb56
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-23 09:28:59 +0530

    add devcamper_api master

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,17 +3,30 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-    console.log(req.body);
-    res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+    } catch (error) {
+        res.status(400).json({ success: false, error: error.message });
+    }
 };
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-    console.log(req.body);
-    res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            return res.status(404).json({ success: false, msg: 'Bootcamp not found' });
+        }
+
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (error) {
+        res.status(400).json({ success: false, error: error.message });
+    }
 };
 
 // @desc    Create new bootcamp
