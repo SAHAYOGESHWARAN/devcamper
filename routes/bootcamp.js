2988d7c1ea9125b515a08f8de3d21117518b1441
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-25 08:39:07 +0530

    add devcamper_api master

--- routes/bootcamp.js
+++ routes/bootcamp.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps');
+const {
+    getBootcamps,
+    getBootcamp,
+    createBootcamp,
+    updateBootcamp,
+    deleteBootcamp
+} = require('../controllers/bootcamps');
 
 const router = express.Router();
 
