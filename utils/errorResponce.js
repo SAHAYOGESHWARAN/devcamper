80be20a7e752be4ada97c015e2811ec847c56a2f
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-25 08:01:58 +0530

    add devcamper_api master

--- utils/errorResponce.js
+++ utils/errorResponce.js
@@ -1,4 +1,9 @@
-class ErrorRespponse extends Error {
+class ErrorResponse extends Error {
     constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
 }
 }
+
+
+module.exports =ErrorResponse
