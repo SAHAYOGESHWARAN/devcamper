14239fc2a1fe498edbbf707695063ba569669c3b
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-25 09:05:59 +0530

    add devcamper_api master

--- middleware/error.js
+++ middleware/error.js
@@ -1,34 +1,26 @@
 const ErrorResponse = require('../utils/ErrorResponse');
 
-const errorHandler = (err, req, res, next) => {
-    let error = { ...err };
-    error.message = err.message;
+const errorHandler =(err,req,res,next) => {
+ let error = {...err}
 
-    // Log to console for dev
-    console.log(err.stack);
+ error.message = err .message;
+    //log to console for dev
+    console.log(err.stack.red);
 
-    // Mongoose bad ObjectId
-    if (err.name === 'CastError') {
-        const message = `Resource not found`;
-        error = new ErrorResponse(message, 404);
-    }
 
-    // Mongoose duplicate key
-    if (err.code === 11000) {
-        const message = 'Duplicate field value entered';
-        error = new ErrorResponse(message, 400);
-    }
 
-    // Mongoose validation error
-    if (err.name === 'ValidationError') {
-        const message = Object.values(err.errors).map(val => val.message);
-        error = new ErrorResponse(message, 400);
+    // mongoose bad object id
+    if(err.name === 'CastError') {
+        const message ='Bootcamp not found with id of ${err.value}';
+        error = new ErrorResponse(message,404);
+
     }
 
     res.status(error.statusCode || 500).json({
-        success: false,
-        error: error.message || 'Server Error'
+        success:false,
+        error:err.message || 'server Error'
     });
+
 };
 
 module.exports = errorHandler;
